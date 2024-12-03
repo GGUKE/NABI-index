81984b76deaf6b21be0a3d2816cade5ef158b944
GGUKE <pllbong2020@naver.com>
2024-12-03 14:59:10 +0900

    js 수정[반응형 필요]

--- js/main.js
+++ js/main.js
@@ -1,88 +1,67 @@
-$(function(){
-	$(".nav > ul > li").hover(
-		function(){
-			$(this).find("ul").stop().slideDown(300);
-		},
-		function(){
-			$(this).find("ul").stop().slideUp(300);
-		}
-	);
+document.addEventListener("DOMContentLoaded", () => {
+	
+	document.querySelectorAll(".nav > ul > li").forEach(item => {
 
-	$(".nav > ul > li > a").focusin(function(){
-		$(this).parent().addClass("active");
-		$(this).next("ul").slideDown(300);
+		item.addEventListener("mouseenter", () => {
+			const subMenu = item.querySelector("ul");
+			if (subMenu) subMenu.style.display = "block";
+		});
+	 	item.addEventListener("mouseleave", () => {
+			const subMenu = item.querySelector("ul");
+			if (subMenu) subMenu.style.display = "none";
+		});
 	});
-
-	$(".nav ul ul li:last-child a").focusout(function(){
-		$(this).parent().parent().parent().removeClass("active");
-		$(this).parent().parent().slideUp(300);
+  
+	document.querySelectorAll(".nav > ul > li > a").forEach(link => {
+		link.addEventListener("focusin", () => {
+			link.parentElement.classList.add("active");
+			const subMenu = link.nextElementSibling;
+			if (subMenu) subMenu.style.display = "block";
+		});
 	});
+  
 
-	let n=0;
-	let index=0;
-	let total=4
-
-	$(".slider li").eq(n).addClass("active");
-	$(".slider .control li").eq(n).addClass("active");
-
-	function setGallery(){
-		$(".slider li").removeClass("active");
-		$(".slider li").eq(n).addClass("active");
-		$(".control li").removeClass("active");
-		$(".slider .control li").eq(n).addClass("active");
+	let n = 0;
+	const sliderItems = document.querySelectorAll(".slider .gallery ul li");
+	const controlItems = document.querySelectorAll(".slider .control li");
+	
+	function setGallery() {
+		sliderItems.forEach(item => item.classList.remove("active"));
+		sliderItems[n].classList.add("active");
+		controlItems.forEach(item => item.classList.remove("active"));
+		controlItems[n].classList.add("active");
 	}
+	
+	controlItems.forEach((control, index) => {
+		control.addEventListener("click", (e) => {
+			e.preventDefault();
 
-	function intervalMoving(){
-		if(n < (total-1)){
-			n=n+1;
-		}
-		else{
-			n=0;
-		}
-		setGallery();
-	}
+			if (n !== index) {
+					n = index;
+					setGallery();
+			}
 
-	// let id=setInterval(intervalMoving, 5000);
+		});
 
-	$(".slider .control li").click(function(e){
+	});
+	
+	setGallery();
+  
+	let topButton = document.getElementById("top");
+	let lnbClick = document.querySelector(".lnb .close");
+
+	window.addEventListener("scroll", () => {
+	  	topButton.style.display = window.scrollY > 100 ? "block" : "none";
+	});
+  
+	topButton.addEventListener("click", () => window.scrollTo({ top: 0, behavior: "smooth" }));
+  
+
+	lnbClick.addEventListener("click", (e) => {
 		e.preventDefault();
-		index=$(this).index();
 
-		if(n != index){
-			n=index;
-			setGallery();
-		}
+		lnbClick.classList.toggle("active");
 	});
 
-	$(".slider .control li").hover(
-		function(){
-			clearInterval(id);
-		},
-		function(){
-			id=setInterval(intervalMoving, 5000);
-		}
-	);
-
-	$(".lnb .close").click(function(e){
-		e.preventDefault();
-		$(this).toggleClass("active");
-	});
-
-	let t=0;
-
-	$(window).scroll(function(){
-		t=$(window).scrollTop();
-
-		if(t > 100){
-			$("#top").fadeIn(300);
-		}
-		else{
-			$("#top").fadeOut(300);
-		}
-	});
-
-	$("#top").click(function(e){
-		e.preventDefault();
-		$("html").delay(300).animate({ scrollTop: 0 }, 500);
-	});
-});
+  });
+  
